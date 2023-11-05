9106c0d044ad6d08a34b8823414f4caaaeae9189
Harlequine <cs.joshuakylepocampo@gmail.com>
2023-11-05 15:40:11 +0800

    first game done

--- src/components/Games/Sequence-Test/GameGrid.tsx
+++ src/components/Games/Sequence-Test/GameGrid.tsx
@@ -1,17 +1,19 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Dispatch, SetStateAction } from 'react'
 import { useInterval } from 'usehooks-ts'
 
 import './GameGrid.css'
 
 /**
  * TODO:
- * random sequence generator function >
- * sequence checker >
- * correct and incorrect sequence indicator CSS
- * incrementing level >
- * try again
- * clickstate css
+ * audio per click and prompt
  */
+
+interface sequenceTestProps{
+  setGameOver: Dispatch<SetStateAction<boolean>>,
+  getLevel: Dispatch<SetStateAction<number>>,
+  setPromptCorrect:Dispatch<SetStateAction<boolean>>,
+  promptCorrect:boolean,
+}
 
 const randomNumberGenerator = (level:number):number => {
   return Math.floor(1 + Math.random() * level) 
@@ -19,38 +21,41 @@ const randomNumberGenerator = (level:number):number => {
 
 const board = [1,2,3,4,5,6,7,8,9]
 
-const GameGrid = () => {
+const GameGrid = (props:sequenceTestProps) => {
+  const { getLevel, setGameOver, setPromptCorrect, promptCorrect} = props;
+
   const [ sequence, setSeqeunce ] = useState([randomNumberGenerator(9)]);
   const [ playerSequnce, setPlayerSequence ] = useState<number[]>([]);
   const [ psIndex, setPsIndex ] = useState(0);
   const [ currTile, setCurrTile ] = useState(-1);
   const [ showSequence, setShowSequnce ] = useState(true);
   const [ level, setLevel ] = useState(1)
-  const [ promptCorrect, setPromptCorrect ] = useState(false)
-  const [ promptWrong, setPromptWrong ] = useState(false)
-
-  // useEffect(() => {
-  //   sequence.length < 1 && setSeqeunce(curr => [...curr, randomNumberGenerator(1)])
-  // },[])
+  const [ isClicked, setIsClicked ] = useState(0)
 
   useInterval(() => {//function for showing sequence
     currTile > level-1 ? setShowSequnce(false) : setCurrTile(currTile+1);
   }, showSequence ? 500 : null)
 
+
   useEffect(() => {
     setCurrTile(-1)
     console.log(sequence)
-    setPromptWrong(false)
   },[showSequence])
 
   useEffect(() => {
-    setShowSequnce(true)
+    setTimeout(() => {
+      setShowSequnce(true)
+    },500)
   },[sequence])
 
   useEffect(() => {
+    isClicked > 0 && setTimeout(() => { setIsClicked(0)}, 150)
+  },[isClicked])
+
+  useEffect(() => {
+    getLevel(level)
     level > 1 && setSeqeunce((curr:number[]) => [...curr, randomNumberGenerator(9)])
     setPromptCorrect(false)
-    setPromptWrong(false)
   },[level])
 
   useEffect(() => {
@@ -58,43 +63,36 @@ const GameGrid = () => {
   },[playerSequnce])
 
   useEffect(() => {
-    promptCorrect === true && setLevel(level + 1)
-    console.log('correct')
-    setPlayerSequence([])
-    setPsIndex(0);
+    if(promptCorrect === true){
+      setLevel(level + 1)
+      console.log('correct')
+      setPlayerSequence([])
+      setPsIndex(0);
+    }
   },[promptCorrect])
 
-  const inputChecker = (cell:number):void => {//should check per input not as a whole
+  const inputChecker = (cell:number):void => {
+    setIsClicked(cell)
     if(cell !== sequence[psIndex]){
       setPsIndex(0);
       setPlayerSequence([]);
-      setPromptWrong(true);
       console.log('wrong')
+      setGameOver(true)
       return;
     }
     setPlayerSequence((curr:number[]) => [...curr, cell])
   }
 
 
-
   return (
     <>
-      <h2>Level {level}</h2>
+      <h2 className='level'><span>Level:</span> {level}</h2>
       <div className='game-grid'>
         {board.map((cell,idx) => (
-          <div className={sequence[currTile] === cell ? 'cell white' : 'cell'} key={idx} onClick={(() => {!showSequence && 
+          <div className={sequence[currTile] === cell  || isClicked === cell ? 'cell white' : 'cell'} key={idx} onClick={(() => {!showSequence && 
             inputChecker(cell)})}></div>
         ))}
       </div>
-      {promptWrong && 
-        <button onClick={() => setShowSequnce(true)}>
-        try again
-        </button>
-      }
-      {playerSequnce.map((cell,idx) => (<p key={idx}>{}{cell}</p>))}
-      {/* <button onClick={() => setLevel(level+1)}>
-      {level}+1
-      </button> */}
     </>
   )
 }
--- src/pages/Sequence-Test/SequenceTest.tsx
+++ src/pages/Sequence-Test/SequenceTest.tsx
@@ -7,39 +7,59 @@ import './SequenceTest.css'
 
 
 const SequenceTest = () => {
-  const [ points, setPoints ] = useState(0);
   const [ isPlaying, setIsPlaying ] = useState(false);
   const [ gameOver, setGameOver ] = useState(false)
+  const [ level, getLevel ] = useState<number>(0);
+  const [ promptCorrect, setPromptCorrect ] = useState(false)
+  const [ classPrompt, setClassPrompt ] = useState('');
+
+
+
+
+  useEffect(() => {
+    if(promptCorrect === true){
+      setClassPrompt('correct');
+    }
+
+    if(gameOver === true){
+      setClassPrompt('incorrect')
+    }
+  },[promptCorrect,gameOver])
+
+  useEffect(() => {
+    if(classPrompt.length > 0){
+      setTimeout(() => {setClassPrompt('')},500)
+    }
+  },[classPrompt])
 
   return (
-    <div id='sequence-test' className='game-bg'>
+    <div id='sequence-test' className={`game-bg ${classPrompt}`}>
       <div className="container">
         {!isPlaying && 
-          <>
+          <div className='start-game'>
             <svg> <SquaresPlusIcon /> </svg>
             <h1>Sequence Memory Test</h1>
             <h2>Memorize the pattern.</h2>
             <div className="game-btn">
               <a href="#" onClick={() => setIsPlaying(true)}>Start</a>
             </div>
-          </>
+          </div>
         }
 
-        {isPlaying &&
-          <GameGrid/>
+        {(isPlaying && !gameOver) &&
+          <GameGrid getLevel={getLevel} setGameOver={setGameOver} setPromptCorrect={setPromptCorrect} promptCorrect={promptCorrect}/>
         }
-        {/* {gameOver && 
-          <>
+        {gameOver && 
+          <div className='game-over'>
             <svg> <SquaresPlusIcon /> </svg>
-            <h1>Sequence Memory Test</h1>
-            <h2>Memorize the pattern.</h2>
+            <h2>Sequence Memory Test</h2>
+            <h1>Level {level}</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
             <div className="game-btn">
-              <a href="#" onClick={() => setStartGame(true)}>Start</a>
+              <a href="#" onClick={() => {setGameOver(false)}}>Try Again</a>
             </div>
-          </>
-        } */}
-
-
+          </div>
+        }
 
       </div>
     </div>
